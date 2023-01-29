0b7291e7cfc904a6685d9f0d0b1934b23ef33cf8
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-01-29 18:18:49 +0200

    maked radiobroadcaster record

--- src/main/java/personality/RadioBroadcaster.java
+++ src/main/java/personality/RadioBroadcaster.java
@@ -1,39 +1,15 @@
 package personality;
-
-import lombok.AllArgsConstructor;
-import lombok.ToString;
 import translation.Translation;
 
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-@AllArgsConstructor
-@ToString
-public class RadioBroadcaster implements Broadcaster {
-    private String name;
-    private final Set<WorkOnRadioExperience> experience = new LinkedHashSet<>();
-    private final Set<Translation> translations = new LinkedHashSet<>();
-
-    public RadioBroadcaster(String name, Set<WorkOnRadioExperience> experiences, Set<Translation> translations) {
-        this.name = name;
-        addExperiences(experiences);
-        addTranslations(translations);
+public record RadioBroadcaster(String name, Set<WorkOnRadioExperience> experiences, Set<Translation> translations) {
+    public RadioBroadcaster(String name, Set<WorkOnRadioExperience> experiences) {
+        this(name, new LinkedHashSet<>(experiences), new LinkedHashSet<>());
     }
-
-    public void addTranslation(Translation translation) {
-        translations.add(translation);
-    }
-
-    public void addExperience(WorkOnRadioExperience workOnRadioExperience) {
-        experience.add(workOnRadioExperience);
-    }
-
-    public void addTranslations(Set<Translation> translations) {
-        this.translations.addAll(translations);
-    }
-
-    public void addExperiences(Set<WorkOnRadioExperience> experiences) {
-        this.experience.addAll(experiences);
+    public RadioBroadcaster(String name) {
+        this(name, new LinkedHashSet<>(), new LinkedHashSet<>());
     }
 }
