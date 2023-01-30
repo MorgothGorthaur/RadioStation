d3e448913e2222f6c601b9658c3307d55de44dde
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-01-30 16:08:59 +0200

    updated dao

--- src/main/java/dao/DaoImpl.java
+++ src/main/java/dao/DaoImpl.java
@@ -1,24 +1,38 @@
 package dao;
 
+import dao.lexer.Lexer;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import personality.Broadcaster;
+
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.*;
+
 @RequiredArgsConstructor
 public class DaoImpl implements RadioStationDao {
     private final String FILE_NAME;
+    private final Lexer lexer;
+
     @Override
     @SneakyThrows
-    public void write(Deque<Broadcaster> broadcasters){
-        try(var writer = new BufferedWriter(new FileWriter(FILE_NAME))) {
+    public void write(Deque<Broadcaster> broadcasters) {
+        try (var writer = new BufferedWriter(new FileWriter(FILE_NAME))) {
             for (var broadcaster : broadcasters) writer.append(broadcaster.toString()).append("\n");
         }
     }
 
     @Override
+    @SneakyThrows
     public List<Broadcaster> read() {
-     return null;
+        try (var reader = new BufferedReader(new FileReader(FILE_NAME))) {
+            var broadcasters = new LinkedList<Broadcaster>();
+            var line = "";
+            while ((line = reader.readLine()) != null) broadcasters.add(lexer.interpret(line));
+            return broadcasters;
+        }
+
     }
 }
