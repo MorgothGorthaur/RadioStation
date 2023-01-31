3b4e96c6de31ed2190282aeb87e1bc825d2772bf
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-01-31 11:52:21 +0200

    added tests

--- src/test/java/dao/lexer/LexerTest.java
+++ src/test/java/dao/lexer/LexerTest.java
@@ -1,6 +1,8 @@
 package dao.lexer;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import personality.GuestBroadcaster;
 import personality.RadioBroadcaster;
 import personality.WorkOnRadioExperience;
@@ -13,26 +15,41 @@ import translation.part.Music;
 import java.util.LinkedHashSet;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LexerTest {
 
     Lexer lexer = new LexerImpl();
     @Test
-    void testInterpret() {
-        var broadcaster = new GuestBroadcaster("v f", "fff");
-        var translation = new TranslationImpl.Builder(5)
-                .addPart(new Music("singer", "song", 2.3))
-                        .addPart(new Interview("Misha", 1.2))
-                                .addPart(new Advertisement("product fff", 0.8))
-                                        .addPart(new Music("another", "g", 0.7)).build();
-        var translation_2 = new TranslationImpl.Builder(5).addPart(new Music("singer", "song", 2.3))
-                .addPart(new Interview("Vasya", 1.2))
-                .addPart(new Advertisement("product", 0.8))
-                .addPart(new Music("another", "g", 0.7)).build();
+    void testInterpret_shouldReturnRadioBroadcaster() {
+        var inputString = "R, Victor Tarasov, (E, some radio, 5.0=>E, another radio, 6.0=>), T 10.0 15.0 [M, some singer, some music, 5.0=>A, some product, 5.0=>M, another singer, another music, 5.0=>]|";
+        assertThat(lexer.interpret(inputString).toString()).isEqualTo(inputString);
+    }
+    @Test
+    void testInterpret_shouldReturnRadioBroadcaster_withEmptyExperience() {
+        var inputString = "R, Victor Tarasov, (), T 10.0 15.0 [M, some singer, some music, 5.0=>A, some product, 5.0=>M, another singer, another music, 5.0=>]|";
+        assertThat(lexer.interpret(inputString).toString()).isEqualTo(inputString);
+    }
 
-        broadcaster.translations().addAll(List.of(translation, translation_2));
-        assertEquals(lexer.interpret(broadcaster.toString()).toString(), broadcaster.toString());
-        System.out.println(broadcaster);
+    @Test
+    void testInterpret_shouldReturnRadioBroadcaster_withEmptyTranslation() {
+        var inputString = "R, Victor Tarasov, (E, some radio, 5.0=>E, another radio, 6.0=>), ";
+        assertThat(lexer.interpret(inputString).toString()).isEqualTo(inputString);
+    }
+
+    @Test
+    void testInterpret_shouldReturnRadioBroadcaster_withTranslation_withoutAnyParts() {
+        var inputString = "R, Victor Tarasov, (E, some radio, 5.0=>E, another radio, 6.0=>), T 10.0 15.0 []|";
+        assertThat(lexer.interpret(inputString).toString()).isEqualTo(inputString);
+    }
+
+    @Test
+    void testInterpret_shouldReturnGuestBroadcaster() {
+        var inputString = "G, Victor Tarasov, some resume, T 10.0 15.0 [M, some singer, some music, 5.0=>A, some product, 5.0=>M, another singer, another music, 5.0=>]|";
+        assertThat(lexer.interpret(inputString).toString()).isEqualTo(inputString);
+
+        inputString = "G, Victor Tarasov, T 10.0 15.0 [M, some singer, some music, 5.0=>A, some product, 5.0=>M, another singer, another music, 5.0=>]|";
+        assertThat(lexer.interpret(inputString).toString()).isEqualTo(inputString);
     }
 }
