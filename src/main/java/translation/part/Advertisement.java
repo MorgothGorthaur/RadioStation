4f8b92f8b7aa25bc0b71de920091be0fecaaf911
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-01-31 13:38:51 +0200

    refactored advertisement: added exception

--- src/main/java/translation/part/Advertisement.java
+++ src/main/java/translation/part/Advertisement.java
@@ -1,9 +1,16 @@
 package translation.part;
 
-public record Advertisement(String productName, double minuteDuration) implements CommercialPart {
+import exception.AdvertisementCreationException;
+import lombok.NonNull;
+
+public record Advertisement(@NonNull String productName,@NonNull double minuteDuration) implements CommercialPart {
     @Override
     public double getPrice() {
         return 5 * minuteDuration;
+    }
+
+    public Advertisement {
+        if(productName.equals("") || minuteDuration <= 0) throw new AdvertisementCreationException();
     }
 
     @Override
