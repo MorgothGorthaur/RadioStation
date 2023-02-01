92b1259b735455bae43483b35e3c28a5b144da42
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-02-01 21:01:56 +0200

    refactored

--- src/main/java/dao/creators/experience/ExperienceCreator.java
+++ src/main/java/dao/creators/experience/ExperienceCreator.java
@@ -1,7 +1,8 @@
 package dao.creators.experience;
 
+import personality.Experience;
 import personality.WorkOnRadioExperienceImpl;
 
 public interface ExperienceCreator {
-    WorkOnRadioExperienceImpl create();
+    Experience create();
 }
