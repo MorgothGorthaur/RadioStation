16260c17711994533fcd4bd8d2bf139e8b8dc408
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-02-03 17:06:22 +0200

    refactored

--- src/main/java/exception/TooBigCommercialTimeException.java
+++ src/main/java/exception/TooBigCommercialTimeException.java
@@ -2,6 +2,6 @@ package exception;
 
 public class TooBigCommercialTimeException extends RuntimeException {
     public TooBigCommercialTimeException(double commercialTime, double minuteDuration) {
-        super("The commercial time must be not bigger then 50%, but you have: " + Math.round((commercialTime * 100 / minuteDuration) * 100) / 100);
+        super("The commercial time must be not bigger then 50%, but you have: " + Math.round((commercialTime * 100 / minuteDuration) * 100) / 100 + "%");
     }
 }
